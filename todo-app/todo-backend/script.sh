7d525cb1c4aca2af25ba6db6bda8002401663d51
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-10 19:01:23 +0800

    add scripts for docker compose

--- todo-app/todo-backend/script.sh
+++ todo-app/todo-backend/script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -t todo-backend . && docker run -p 3123:3000 todo-backend
+# docker build -t todo-backend . && docker run -p 3123:3000 todo-backend
+
+# docker compose up -d --build
+
+docker compose down
