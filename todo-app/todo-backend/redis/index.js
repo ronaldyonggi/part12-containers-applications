c1d82afdaf303e46acf359b17ad14b4dea41dc84
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-14 18:11:25 +0800

    Prettier formatting

--- todo-app/todo-backend/redis/index.js
+++ todo-app/todo-backend/redis/index.js
@@ -1,28 +1,31 @@
-const redis = require('redis')
-const { promisify } = require('util')
-const { REDIS_URL } = require('../util/config')
+const redis = require('redis');
+const { promisify } = require('util');
+const { REDIS_URL } = require('../util/config');
 
-let getAsync
-let setAsync
+let getAsync;
+let setAsync;
 
 if (!REDIS_URL) {
   const redisIsDisabled = () => {
-    console.log('No REDIS_URL set, Redis is disabled')
-    return null
-  }
-  getAsync = redisIsDisabled
-  setAsync = redisIsDisabled
+    console.log('No REDIS_URL set, Redis is disabled');
+    return null;
+  };
+  getAsync = redisIsDisabled;
+  setAsync = redisIsDisabled;
 } else {
   const client = redis.createClient({
-    url: REDIS_URL
-  })
-    
+    url: REDIS_URL,
+  });
+
+  getAsync = promisify(client.get).bind(client);
+  setAsync = promisify(client.set).bind(client);
+}
 
 const initializeRedisAddedTodos = async () => {
   let added_todos = await getAsync('added_todos');
   if (!added_todos) {
     await setAsync('added_todos', 0);
-}
+  }
 };
 
 module.exports = {
