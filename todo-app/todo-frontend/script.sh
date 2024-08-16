b0b9b95bc8ddc1a9a60975885f71407de4f4291d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-16 14:25:45 +0800

    helper script

---
+++ todo-app/todo-frontend/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker build . -t todo-frontend
+# docker run -p 8000:80 todo-frontend
+# docker build -t todo-frontend-test --progress=plain --no-cache --target test .
+# docker compose up --build -d
+docker compose down
